c2c323badaed7310d20539c7211044d0aa35a0b7
thierrryScotto <thierry_192@outlook.com>
2021-05-12 17:48:27 -0300

    made the authetication in profileBackofficeController and adminsBackofficeController

--- app/controllers/admins_backoffice_controller.rb
+++ app/controllers/admins_backoffice_controller.rb
@@ -1,3 +1,5 @@
 class AdminsBackofficeController < ApplicationController
+  before_action: autheticate_admin!
+
   layout 'admins_backoffice'
 end
--- app/controllers/profiles_backoffice_controller.rb
+++ app/controllers/profiles_backoffice_controller.rb
@@ -1,3 +1,5 @@
 class ProfilesBackofficeController < ApplicationController
+  before_action: autheticate_profile!
+
   layout 'profiles_backoffice'
 end
