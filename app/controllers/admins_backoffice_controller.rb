8145fb30478d9554bac67e6bb492e94bb5fbf40c
thierrryScotto <thierry_192@outlook.com>
2021-05-18 00:37:59 -0300

    added users struct

--- app/controllers/admins_backoffice_controller.rb
+++ app/controllers/admins_backoffice_controller.rb
@@ -1,3 +1,4 @@
 class AdminsBackofficeController < ApplicationController
+  before_action :authenticate_user!
   layout 'admins_backoffice'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+  devise_for :users
   namespace :site do
     get 'welcome/index'
   end
-  namespace :profiles_backoffice do
+  namespace :users_backoffice do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
@@ -14,3 +18,4 @@ Rails.application.routes.draw do
   root to: 'site/welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+ 
