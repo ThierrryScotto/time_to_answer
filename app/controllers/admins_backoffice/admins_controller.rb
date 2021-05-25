607c45cd04abf6ffee06862a4ae6f4f616cd92f8
thierrryScotto <thierry_192@outlook.com>
2021-05-25 00:58:34 -0300

    added update route

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,6 +1,25 @@
-class AdminsBackoffice::AdminsController < ApplicationController
+class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  before_action [:edit, :update], only: [:find_admin]
+
   def index
     @admins = Admin.all
     render json: @admins
+  end
+
+  def edit
+    render json: @admin
+  end
+
+  def update
+    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
+
+    @admin = admin.update(params_admin)
+    render json: @admin
+  end
+
+  private
+
+  def find_admin
+    @admin = Admin.find_by(id: params[:id])
   end
 end
